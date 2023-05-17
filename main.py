0051c96e30c52a8af76b74b2f5ec60f7473b253b
anjankow <anjankow@protonmail.com>
2023-05-17 15:17:59 +0000

    use generated functions in main

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import pandas as pd
 import pytesseract
+import cv2 as cv
+
+from crop_to_user_selection import crop_to_user_selection
+from apply_threshold import apply_threshold
+from draw_word_boxes import draw_word_boxes
 
 file_name = '5.jpeg'
-tsv_file_name = 'test.tsv'
 
-data = pytesseract.image_to_data(file_name, output_type=pytesseract.Output.STRING)
-with open(tsv_file_name, 'w') as f:
-    f.write(data)
+for i in [1,2,4,5]:
+    file_name = f'{i}.jpg'
+    tsv_file_name = f'test{i}.tsv'
 
-df = pd.read_csv(tsv_file_name, sep="\t", lineterminator="\n", verbose=True)
+    processed_image = 'tmp.jpg'
 
-print(df["text"])
-print(df["conf"])
+    print('Processing '+file_name)
+    apply_threshold(file_name, processed_image, 127)
+    print('Applied threshold')
+    draw_word_boxes(processed_image, processed_image, 40)
+    print('Drew word boxes')
+    crop_to_user_selection(processed_image, 'out_'+file_name)
+    print('Cropped to user selection')
+
