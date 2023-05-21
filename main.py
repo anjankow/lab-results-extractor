6ba3a51fa5d9cb91223167d8e8023364b5d165ee
anjankow <anjankow@protonmail.com>
2023-05-21 18:17:29 +0000

    use remove_shadow in main

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import cv2 as cv
 from user_select_image_area import *
 from apply_threshold import apply_threshold
 from draw_word_boxes import draw_word_boxes
+from extract_column_data import *
+from remove_shadow import remove_shadow
 
 file_name = '5.jpeg'
 
-for i in [1,4]:
+for i in [1]:
     file_name = f'{i}.jpg'
     tsv_file_name = f'test{i}.tsv'
 
@@ -19,10 +21,10 @@ for i in [1,4]:
     confidence = 40
 
     print('Processing '+file_name)
-    apply_threshold(file_name, image_to_show, threshold)
     apply_threshold(file_name, processed_image, threshold)
+    remove_shadow(processed_image, processed_image)
     print('Applied threshold')
-    draw_word_boxes(image_to_show, image_to_show, confidence)
+    draw_word_boxes(processed_image, image_to_show, confidence)
     print('Drew word boxes on the image to show')
 
     # get coordinates from image_to_show that has already boxes drawn on it
@@ -34,9 +36,7 @@ for i in [1,4]:
     # 3. Units
     units_coordinates = user_select_image_area(image_to_show, 'Select result units')
 
-    # crop processed_image and only then draw boxes
-    crop_image(processed_image, processed_image, labels_coordinates)
-    print('Cropped to user selection')
+    # Now get data from these selections
     draw_word_boxes(processed_image, processed_image, confidence)
     print('Drew word boxes on user selected area')
 
--- remove_shadow.py
+++ remove_shadow.py
@@ -2,22 +2,27 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('2.jpeg', -1)
+def remove_shadow(image_path, out_image_path):
 
-rgb_planes = cv2.split(img)
+    img = cv2.imread(image_path, -1)
 
-result_planes = []
-result_norm_planes = []
-for plane in rgb_planes:
-    dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
-    bg_img = cv2.medianBlur(dilated_img, 21)
-    diff_img = 255 - cv2.absdiff(plane, bg_img)
-    norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
-    result_planes.append(diff_img)
-    result_norm_planes.append(norm_img)
+    rgb_planes = cv2.split(img)
 
-result = cv2.merge(result_planes)
-result_norm = cv2.merge(result_norm_planes)
+    result_planes = []
+    result_norm_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        norm_img = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+        result_planes.append(diff_img)
+        result_norm_planes.append(norm_img)
 
-cv2.imwrite('shadows_out.png', result)
-cv2.imwrite('shadows_out_norm.png', result_norm)
+    result = cv2.merge(result_planes)
+    result_norm = cv2.merge(result_norm_planes)
+
+    # cv2.imwrite('shadows_out.png', result)
+    cv2.imwrite(out_image_path, result_norm)
+
+
+remove_shadow('2.jpeg', '2_noshadow.jpeg')
